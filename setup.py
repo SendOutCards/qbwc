e095b59aa923ef4ccc4b7bdc362f67a88be0c75d
Tyler Lovely <tyler.n.lovely@gmail.com>
2020-08-03 08:54:49 -0600

    fix: remove package data req

--- setup.py
+++ setup.py
@@ -7,6 +7,5 @@ setup(
 	author='Tyler Lovely',
 	author_email='tyler.n.lovely@gmail.com',
 	packages=find_packages(),
-	install_requires=open('requirements.txt').readlines(),
-	include_package_data=True
+	install_requires=open('requirements.txt').readlines()
 )
