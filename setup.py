27ae5f1ec9b26f99ce397e5be6b23b360f828554
Tyler Lovely <tyler.n.lovely@gmail.com>
2020-08-05 10:58:00 -0600

    chore: inc version 0.1.0 -> 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qbwc',
-    version='0.1.0',
+    version='0.2.0',
     description='QuickBooks Web Connector SDK',
     author='Tyler Lovely',
     author_email='tyler.n.lovely@gmail.com',
